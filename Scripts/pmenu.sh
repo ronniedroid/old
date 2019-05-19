79ad5e2a028140dff681fd8538cc943d3d2fd07a
ronniedroid <ronni.n.yousif@gmail.com>
2019-05-19 01:01:08 -0500

    Add files via upload

--- Scripts/pmenu.sh
+++ Scripts/pmenu.sh
@@ -6,7 +6,7 @@
 MENU="$(rofi -sep "|" -dmenu -i -p ' Power Menu' -location 3 -xoffset -10 -yoffset 50 -width 12 -hide-scrollbar -line-padding 4 -padding 20 -lines 4 <<< " Lock| Logout| Reboot| Shutdown")"
             case "$MENU" in
                 *Lock) i3lock-fancy ;;
-                *Logout) ~/Scripts/i3exit.sh ;;
+                *Logout) ~/Scripts/exit.sh;;
                 *Reboot) ~/Scripts/reboot.sh  ;;
                 *Shutdown) ~/Scripts/poweroff.sh
 esac
