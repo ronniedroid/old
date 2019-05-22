bfe41a1855d377b8aeb7f2d060db95fcd010f880
ronniedroid <ronni.n.yousif@gmail.com>
2019-05-22 16:08:00 +0300

    Updated spme things

--- home/Scripts/exit.sh
+++ home/Scripts/exit.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-## Created By Aditya Shakya
-#MENU="$(rofi -sep "|" -dmenu -i -p 'System' -location 3 -xoffset -10 -yoffset 50 -width 10 -hide-scrollbar -line-padding 4 -padding 20 -lines 4 <<< " Lock| Logout| Reboot| Shutdown")"
-MENU="$(rofi -sep "|" -dmenu -i -p ' Exit i3?' -location 3 -xoffset -10 -yoffset 50 -width 12 -hide-scrollbar -line-padding 3 -padding 20 -lines 2 <<< " YES| NO")"
+MENU="$(rofi -sep "|" -dmenu -i -p ' Exit bspwm?' -location 3 -xoffset -10 -yoffset 50 -width 12 -hide-scrollbar -line-padding 3 -padding 20 -lines 2 <<< " YES| NO")"
             case "$MENU" in
 		 *YES) bspc quit ;;
                  *NO) exit 0
--- home/Scripts/reboot.sh
+++ home/Scripts/reboot.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-## Created By Aditya Shakya
-#MENU="$(rofi -sep "|" -dmenu -i -p 'System' -location 3 -xoffset -10 -yoffset 50 -width 10 -hide-scrollbar -line-padding 4 -padding 20 -lines 4 <<< " Lock| Logout| Reboot| Shutdown")"
 MENU="$(rofi -sep "|" -dmenu -i -p ' Reboot?' -location 3 -xoffset -10 -yoffset 50 -width 12 -hide-scrollbar -line-padding 3 -padding 20 -lines 2 <<< " YES| NO")"
             case "$MENU" in
 		 *YES) systemctl reboot ;;
