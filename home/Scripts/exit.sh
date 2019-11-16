5324ae3f56d11d74f78a3d7a9e5a38c06eeae0dc
ronniedroid <ronni.n.yousif@gmail.com>
2019-11-16 14:20:25 +0300

    Update exit.sh

--- home/Scripts/exit.sh
+++ home/Scripts/exit.sh
@@ -5,13 +5,8 @@ c=$(echo -e "YES\nNO\n" | $MENU)
 
 if [ ${c} == YES ] 
 then
-	  if [ $(wmctrl -m | awk 'NR==1 {print $2}') == i3 ]
-	  then
-		  i3-msg exit
-	  elif [ $(wmctrl -m | awk 'NR==1 {print $2}') == berry ]
-	  then
-		  killall berry
-	  fi 
+	  i3-msg exit
+
 elif [ ${c} == NO ]
 then
 	exit 0
